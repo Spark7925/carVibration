a6c8cfa43b8be740a4d2062194422939327b76b5
binchen <binchen2016@163.com>
2021-11-25 18:56:25 +0800

    陈斌，项目初始配置，20211125

---
+++ vibration/urls.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+version: ***
+author: binchen
+time: 2021-11-25 18:02
+"""
+
+from django.urls import path
+
+
+urlpatterns = [
+    # path(r'merchandise/',ShoppingCenter.as_view(),name="initializePara"),
+]
